c517484b41c8291b48c37ab510aeed8f1c72a462
evagalyuta <e.galiuta@mkskom.ru>
2022-04-17 18:21:31 +0300

    add text fields

--- regexp_helper/src/main/java/org/intellij/sdk/regexp/CheckRegexp.java
+++ regexp_helper/src/main/java/org/intellij/sdk/regexp/CheckRegexp.java
@@ -3,27 +3,57 @@ package org.intellij.sdk.regexp;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
+import org.w3c.dom.Text;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class CheckRegexp extends AnAction {
 
+    private JFrame frame;
+    private JPanel panel;
+    private TextField regexp;
+    private Label textRegExp;
+    private TextField test;
+    private Label textTest;
+
+
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Project project = e.getProject();
+        initialize(e);
+    }
 
-        JFrame frame = new JFrame("RegExp helper");
+    private void initialize(AnActionEvent e){
+        frame = new JFrame("RegExp helper");
 
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(250, 150));
+        regexp = new TextField();
+        regexp.setPreferredSize(new Dimension(200, 30));
+        regexp.setBackground(frame.getBackground());
+        textRegExp = new Label("Your RegExp: ");
+        textRegExp.setSize(100, 30);
+        textRegExp.setAlignment(Label.LEFT);
+
+        test = new TextField();
+        test.setPreferredSize(new Dimension(200, 30));
+        test.setBackground(frame.getBackground());
+        textTest = new Label("Test: ");
+        textTest.setSize(100, 30);
+        textTest.setAlignment(Label.LEFT);
+
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(300, 150));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(textRegExp);
+        panel.add(regexp);
+        panel.add(textTest);
+        panel.add(test);
+
+
         frame.add(panel);
-
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setAlwaysOnTop(true);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        //frame.setSize(300, 200);
         frame.pack();
     }
 }
